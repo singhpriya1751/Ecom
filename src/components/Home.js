b36d676b41d1f7a029204509a09d69715e24caed
kapish4792 <sravish551@gmail.com>
2023-02-15 13:07:38 +0530

    ui fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
-import React from "react";
-import Newsection from "./Newsection";
-import Parts from "./Parts";
-import Services from "./Services";
-import Footer from "./Footer";
+import React from 'react';
+import Newsection from './Newsection';
+import Parts from './Parts';
+import Services from './Services';
+import Footer from './Footer';
 function Home() {
   const data = {
-    name: "CLOTHING STORE",
+    name: 'CLOTHING STORE',
   };
   return (
     <>
-      <Newsection myData={data} />
-      <Services />
-      <Parts />
-      <Footer />
-      <h1> Welcome</h1>
+      <div className="container mt-4">
+        <Newsection myData={data} />
+        <Services />
+        <Parts />
+        <Footer />
+        <h1> Welcome</h1>
+      </div>
     </>
   );
 }
